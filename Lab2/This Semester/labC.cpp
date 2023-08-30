b1d2a839d9ebbc92fbd3684bc17868dfa729893b
Baymax <118838868+BayMax-6@users.noreply.github.com>
2023-08-30 09:40:13 -0500

    Add files via upload

--- Lab2/This Semester/labC.cpp
+++ Lab2/This Semester/labC.cpp
@@ -1,36 +1,26 @@
 /*
-    name: April Crockett and Modified by Addison Goforth
+    name: Addison Goforth
     Date: 8/28/23
     purpose: Debugging Example
     files: labA.cpp
 */
 
 #include <iostream> 
+#include <iomanip>
 using namespace std;
 
 int main(){
 
-	string firstName1;
-	string lastName1;
-	string firstName2;
-	string lastName2;
+	double quotient = 132.36485967;
+	
+	cout << setprecision(1) << fixed << quotient << endl;
 
-	cout << "What is the first person's first name?" << endl;
-	cin >> firstName1;
-	cout << "What is the first person's last name?" << endl;
-	cin >> lastName1;
-
-	cout << "What is the second person's first name?" << endl;
-	cin >> firstName2;
-	cout << "What is the second person's last name?" << endl;
-	cin >> lastName2;
-
-	cout << "Here are some common married-couple names:" << endl;
-	cout << firstName1 << " " << lastName1 << " and " << firstName2 << " " << lastName2 << endl;
-	cout << firstName1 << " and " << firstName2 << " " << lastName1 << endl;
-	cout << firstName1 << " and " << firstName2 << " " << lastName2 << endl;
-	// FIXME: Print two hyphenated last name options, with either last name
-	//        appearing first. (A hyphen can be written as "-")
+	cout << quotient << endl;
+	cout << setprecision(5) << quotient << endl;
+	cout << setprecision(4) << quotient << endl;
+	cout << setprecision(3) << quotient << endl;
+	cout << setprecision(2) << quotient << endl;
+	cout << setprecision(1) << quotient << endl;
 
 	return 0;
 }
