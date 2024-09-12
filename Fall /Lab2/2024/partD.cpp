8afb8843e2e718437f940a993397815bb305f21c
addison goforth <addison@QWRkaXNvbnMtTWFjQm9vay1Qcm8.local>
2024-09-12 16:39:55 -0500

    Adding Getline

--- Fall /Lab2/2024/partD.cpp
+++ Fall /Lab2/2024/partD.cpp
@@ -6,16 +6,22 @@ purpose: lab 2 example
 */
 
 #include <iostream>
+#include <string>
 using namespace std; 
 
 int main(){ 
 
     //defining variables 
-    
+    string name; 
+    int age; 
     //cin, cout, and equaltions 
-    
+    cout << "What is your name: "; 
+    getline(cin, name); 
 
+    cout << "How old are you: "; 
+    cin >> age; 
+
+    //do we need cin.ignore()
     //return statement 
     return 0; 
-
 }
